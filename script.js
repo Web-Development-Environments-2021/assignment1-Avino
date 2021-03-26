6e1a78c6d25c55b278172df644effddd79fee27b
Ofer Avin <oferavin32@gmail.com>
2021-03-26 17:04:01 +0300

    still fonts

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-// function isInViewport(element) {
-//     const rect = element.getBoundingClientRect();
-//     return (
-//         rect.top >= 0 &&
-//         rect.left >= 0 &&
-//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
+
+
 window.onscroll = function() {stickyFunction()};
 
-var navbar = document.getElementById("menu");
-var sticky = navbar.offsetTop;
+let navbar = document.getElementById("menu");
+let sticky = navbar.offsetTop;
 
 function stickyFunction() {
-    console.log(sticky)
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
